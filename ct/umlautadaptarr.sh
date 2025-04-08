9d8957c70d391ce264ca015ac45a29a62f9dc006
elvito <elvito@users.noreply.github.com>
2025-04-08 20:54:42 +0200

    Update umlautadaptarr.sh
    build.func auf eigenes repo angepasst

--- ct/umlautadaptarr.sh
+++ ct/umlautadaptarr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/[USER]/[REPO]/refs/heads/[BRANCH]/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/elvito/ProxmoxVE/refs/heads/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: [YourUserName]
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
